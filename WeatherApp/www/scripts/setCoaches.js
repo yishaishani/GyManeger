df8382b67819d4d5c14105d40273b2bfee61f3fe
yishai-pc\yishaishani <y0547387610@gmail.com>
2017-05-25 16:25:01 +0300

    bring all coaches to div(with out '\n')

--- WeatherApp/www/scripts/setCoaches.js
+++ WeatherApp/www/scripts/setCoaches.js
@@ -3,29 +3,41 @@
     var lestName = document.getElementById("LastName").value;
     var dayOfWork = document.getElementById("DayOfWork").value;
    
-    $.ajax({
-        url: "http://y0547387610-001-site1.ctempurl.com/Coaches/" + firstName + "/" + lestName + "/" + dayOfWork,
 
-    });
-    
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://y0547387610-001-site1.ctempurl.com/Coaches/" + firstName + "/" + lestName + "/" + dayOfWork, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(this.response);
+        }
+    }
+           
 }
 
 function getCoaches() {
     //var li = document.createElement("li");
     var divCoaches = document.getElementById("getCoaches");
-    alert("hallo");
+    
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://y0547387610-001-site1.ctempurl.com/Coaches/all", true);
     xhttp.send();
+
        xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("getCoaches").innerHTML =
-                this.responseText;
+                var data = [];
+                data = this.responseText
+                var size = data.length;
+
+                document.getElementById("getCoaches").innerHTML += 'name:';
+                for (i = 0; i < size; i++) {
+                    if (data[i] == '*')
+                        document.getElementById("getCoaches").innerHTML += '\n'+'name:' ;
+                    else
+                        document.getElementById("getCoaches").innerHTML += data[i];
+                }
             }
-        };
+       };
+    
         
-        
-    //$.ajax({
-    //    url: "http://y0547387610-001-site1.ctempurl.com/Coaches/all",  
-    //});
 }
