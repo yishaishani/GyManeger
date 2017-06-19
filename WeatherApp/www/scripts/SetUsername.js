e620b0d9f6679c9967fc552bf6e6657e7b6e0200
yishai-pc\yishaishani <y0547387610@gmail.com>
2017-06-19 23:31:03 +0300

    start verify password.

--- WeatherApp/www/scripts/SetUsername.js
+++ WeatherApp/www/scripts/SetUsername.js
@@ -1,21 +1,41 @@
 ﻿
 function SetUsername() {
     
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
- 
-   
+    var username = document.getElementById("usernameLOG").value;
+    var password = document.getElementById("passwordLOG").value;
+     
+
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://yishai-001-site1.atempurl.com/Members/" + username + "/" + password, true);
-    xhttp.send();
+    xhttp.open("POST", "http://yishai-001-site1.atempurl.com/Members/" + username, true);
+
+    xhttp.send(password);
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             alert(this.response);
-            window.location.href = "#main-members"
+            window.location.href = "#main-members";
         }
     }
 }
 
+function verifyUserName() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://yishai-001-site1.atempurl.com/Members/" + username + "/" + password, true);
+
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.response == true)
+                window.location.href = "#opening-pop";
+            else
+                window.location.href = "#about-pop";
+        }
+    }
+
+
+}
 
 function getMembers() {
     var xhttp = new XMLHttpRequest();
